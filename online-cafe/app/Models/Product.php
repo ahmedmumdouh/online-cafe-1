5a54cad5417384da8cb875c0469f7d104499f37a
Abobakr <abobakryousre@gmail.com>
2021-04-15 22:08:06 +0200

    feat(database-schema): apply many to many realtion between orders and products

--- online-cafe/app/Models/Product.php
+++ online-cafe/app/Models/Product.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Order;
 
 class Product extends Model
 {
     use HasFactory;
+
+    public function orders()
+    {
+        return $this->belongsToMany(User::class)->withPivot(['quantity']);
+    }
 }

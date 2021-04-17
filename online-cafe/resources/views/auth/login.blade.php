b5f42925a4f66f4c1db638f0a4e5ffff8b8c3c25
Abobakr <abobakryousre@gmail.com>
2021-04-17 23:35:52 +0200

    feat(social-authentication): login wiht google, githup, facebook

--- online-cafe/resources/views/auth/login.blade.php
+++ online-cafe/resources/views/auth/login.blade.php
@@ -18,9 +18,9 @@
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -32,9 +32,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -58,11 +58,24 @@
                                 </button>
 
                                 @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    {{ __('Forgot Your Password?') }}
+                                </a>
                                 @endif
                             </div>
+
+                            <!-- login with social media -->
+                        </div>
+                        <div class="form-group row mb-0 justify-content-center  text-center">
+                            <div class="col-6  ">
+                                <a href="{{ route('login.google') }}" class="  btn btn-sm btn-lg "> <i class="fab fa-google btn btn-lg text-danger"></i>
+                                </a>
+                                <a href="{{ route('login.facebook') }}" class="  btn  btn-lg"> <i class="fab fa-facebook-f fab-lg btn btn-lg  text-primary"></i>
+                                </a>
+                                <a href="{{ route('login.github') }}" class=" btn btn-lg "><i class="fab fa-github fab-lg btn btn-lg fa-5x"></i>
+                                </a>
+                            </div>
+
                         </div>
                     </form>
                 </div>
--- online-cafe/resources/views/layouts/app.blade.php
+++ online-cafe/resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -26,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ _('online-cafe') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -55,6 +56,7 @@
                         @endif
                         @else
                         <li class="nav-item dropdown">
+                            <img src="{{ Auth::user()->avatar }}" alt="{{ Auth::user()->name }}" />
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                 {{ Auth::user()->name }}
                             </a>

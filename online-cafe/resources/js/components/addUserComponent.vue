588eec21692178eaa2208c9c4e716ab57477c580
GhadaRagab123 <ghadar109@gmail.com>
2021-04-22 04:24:22 +0200

    feat(feature-admin-add-user):add fun to add photo

--- online-cafe/resources/js/components/addUserComponent.vue
+++ online-cafe/resources/js/components/addUserComponent.vue
@@ -4,21 +4,31 @@
         <form v-on:submit.prevent="adduser">
             <div class="form-group">
                 <lable>Name</lable>
-                <input type="text" class="form-control" name="name" v-model="user.name" />
+                <input type="text" class="form-control" name="name" v-model="form.name" />
             </div>
 
             <div class="form-group">
                 <lable>email</lable>
-                <input type="email" class="form-control" name="email" v-model="user.email" />
+                <input type="email" class="form-control" name="email" v-model="form.email" />
             </div>
 
-            <div class="form-group">
+
+            <div class="custom-file mb-3">
+                        <input type="file" class="custom-file-input" v-on:change="onChange" id="validatedCustomFile" accept="image/*"  required>
+                        <label class="custom-file-label"  for="validatedCustomFile">{{imageName}}</label>
+                        <div class="invalid-feedback">Cover Photo</div> 
+                        <div id="preview" style="margin-top: 1%;">
+                            <img v-if="url" :src="url" />
+                        </div>
+                    </div>
+
+            <!-- <div class="form-group">
                 <lable>Room number</lable>
-                <select name="room_id" class="form-control " >
+                <select name="room_id" class="form-control " > -->
 
                   <!--  <option v-for="room in rooms" :value="room.id">{{room['number']}}</option>  -->
-                </select>
-            </div>
+                <!-- </select>
+            </div> -->
             <button type="submit" name="submit" class="btn btn-primary">create</button>
         </form>
     </div>
@@ -35,20 +45,61 @@ axios.defaults.baseURL = 'http://localhost:8000'
  export default {
         data() {
             return {
-                user: {}
+                // user: {}
+                
+
+              form:{
+                        name:'',
+                        image:null
+                    },
+                    imageName:'Upload Cover Photo',
+                    url: null,
             }
         },
      
         methods: {
-            adduser() {
-                    console.log(this.user);
-                    // console.log(this.$route.params.id)
-                axios
-                    .post("/api/userstore", this.user)
-                    .then((res) => {
-                        this.$router.push({ name:'alluser'});
+
+          onChange(e) {
+                this.form.image = e.target.files[0];
+                this.imageName = e.target.files[0].name; 
+                this.url = URL.createObjectURL(this.form.image);
+                console.log(this.form);
+            },
+
+             onSubmit(e) {
+                e.preventDefault();
+                const existingObj = this;
+                const config = {
+                    headers: {
+                        'content-type': 'multipart/form-data'
+                    }
+                }            
+                const formData = new FormData()
+                formData.append('image', this.form.image)
+                formData.append('name', this.form.name)
+              
+                axios.post("/api/userstore", formData,config).then(function (res) {
+                        existingObj.success = res.data.success;
+                        existingObj.$router.push({ name: 'alluser' })
+                        console.log(res);
+                    })
+                    .catch(function (err) {
+                        existingObj.output = err;
                     });
-            }
+            }  
         }
-    }
+
+
+
+            // adduser() {
+            //         console.log(this.user);
+            //         // console.log(this.$route.params.id)
+            //     axios
+            //         .post("/api/userstore", this.user)
+            //         .then((res) => {
+            //             this.$router.push({ name:'alluser'});
+            //         });
+            // }
+        }
+    
 </script>
